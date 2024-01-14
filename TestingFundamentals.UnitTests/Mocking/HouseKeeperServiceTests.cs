e98d0e9956b19f7df813ff28599980b9d076062b
shakerkamal <shakerkamal18@gmail.com>
2024-01-14 19:43:48 +0100

    Bug fixes on HouseKeeper Service and written unit tests to test the service

---
+++ TestingFundamentals.UnitTests/Mocking/HouseKeeperServiceTests.cs
@@ -0,0 +1,50 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestingFundamentals.Mocking;
+
+namespace TestingFundamentals.UnitTests.Mocking;
+
+[TestFixture]
+public class HouseKeeperServiceTests
+{
+    private Mock<IUnitOfWork> _unitOfWork;
+    private Mock<IStatementGenerator> _statementGenerator;
+    private Mock<IEmailSender> _emailSender;
+    private Mock<IXtraMessageBox> _messageBox;
+    private HouseKeeperService _service;
+    private DateTime _statementDate = new DateTime(2022, 1, 1);
+    private HouseKeeper _houseKeeper;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _houseKeeper = new HouseKeeper { Email = "a", FullName = "b", Oid = 1, StatementEmailBody = "c" };
+        _unitOfWork = new Mock<IUnitOfWork>();
+        _unitOfWork.Setup(uow => uow.Query<HouseKeeper>()).Returns(new List<HouseKeeper>
+        {
+           _houseKeeper
+        }.AsQueryable());
+        _statementGenerator = new Mock<IStatementGenerator>();
+        _emailSender = new Mock<IEmailSender>();
+        _messageBox = new Mock<IXtraMessageBox>();
+        _service = new HouseKeeperService(
+            _unitOfWork.Object,
+            _statementGenerator.Object,
+            _emailSender.Object,
+            _messageBox.Object);
+    }
+
+    [Test]
+    public void SendStatementEmails_WhenCalled_GenerateStatements()
+    {
+        _service.SendStatementEmails(_statementDate);
+
+        _statementGenerator.Verify(sg => 
+            sg.SaveStatement(_houseKeeper.Oid, _houseKeeper.FullName, _statementDate));
+    }
+}
+ 
--- TestingFundamentals/Mocking/HouseKeeperService.cs
+++ TestingFundamentals/Mocking/HouseKeeperService.cs
@@ -1,33 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Globalization;
-using System.Linq;
-using System.Net;
-using System.Net.Mail;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace TestingFundamentals.Mocking;
 
-namespace TestingFundamentals.Mocking;
-
-public class HouseKeeperHelper
+public class HouseKeeperService
 {
     private readonly IUnitOfWork _unitOfWork;
     private readonly IStatementGenerator _statementGenerator;
     private readonly IEmailSender _emailSender;
+    private readonly IXtraMessageBox _messageBox;
 
-    public HouseKeeperHelper(IUnitOfWork unitOfWork, IStatementGenerator statementGenerator, IEmailSender emailSender)
+    public HouseKeeperService(
+        IUnitOfWork unitOfWork, 
+        IStatementGenerator statementGenerator, 
+        IEmailSender emailSender,
+        IXtraMessageBox messageBox)
     {
         _unitOfWork = unitOfWork;
         _statementGenerator = statementGenerator;
         _emailSender = emailSender;
+        _messageBox = messageBox;
     }
 
-    public bool SendStatementEmails(DateTime statementDate)
+    public void SendStatementEmails(DateTime statementDate)
     {
         var houseKeepers = _unitOfWork.Query<HouseKeeper>();
-
-        foreach(var  houseKeeper in houseKeepers)
+        foreach(var houseKeeper in houseKeepers)
         {
             if (houseKeeper.Email is null)
                 continue;
@@ -43,13 +38,11 @@ public class HouseKeeperHelper
             try
             {
                 _emailSender.EmailFile(emailAddress, emailBody, statementFilename,
-                    string.Format("Sandpiper Statement {0:yyyy-MM} {1}", statementDate, houseKeeper.FullName));
-                return true;
+                    string.Format("Sandpiper Statement {0:yyyy-MM} {1}", statementDate, houseKeeper.FullName)); 
             }
             catch(Exception ex)
             {
-                XtraMessageBox.Show(ex.Message, string.Format("Email failure: {0}",emailAddress),MessageBoxButtons.OK);
-                return false;
+                _messageBox.Show(ex.Message, string.Format("Email failure: {0}",emailAddress),MessageBoxButtons.OK);
             }
         }
     }
@@ -59,9 +52,14 @@ public enum MessageBoxButtons
     OK
 }
 
-public class XtraMessageBox
+public interface IXtraMessageBox
 {
-    public static void Show(string s, string houseKeeperStatements, MessageBoxButtons ok)
+    void Show(string s, string houseKeeperStatements, MessageBoxButtons ok);
+}
+
+public class XtraMessageBox : IXtraMessageBox
+{
+    public void Show(string s, string houseKeeperStatements, MessageBoxButtons ok)
     {
     }
 }
@@ -74,7 +72,7 @@ public class MainForm
 public class HouseKeeper
 {
     public int Oid { get; set; }
-    public string FullName { get; set; }
-    public string Email { get; set; }
-    public string StatementEmailBody { get; set; }
+    public string FullName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string StatementEmailBody { get; set; } = string.Empty;
 }
